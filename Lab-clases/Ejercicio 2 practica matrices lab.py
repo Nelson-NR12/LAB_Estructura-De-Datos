d36041a34596fdcb4814c263fd8cd934d816147e
Nelson-NR12 <121265825+Nelson-NR12@users.noreply.github.com>
2023-01-03 16:15:12 -0500

    deleted:    Lab-clases/Ejercicio 1 practica.py 	deleted:    Lab-clases/Ejercicio 2 practica matrices lab.py 	deleted:    Lab-clases/ejercico de practica.py

--- Lab-clases/Ejercicio 2 practica matrices lab.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-matrizA = np.array([[1,2,3],[4,5,6],[7,8,9]], float)
-matrizB = np.array([[9,8,7],[6,5,4],[3,2,1]], float)
-
-suma = matrizA + matrizB
-resta = matrizA - matrizB
-multiplicacion = matrizA*matrizB
-print(f"Suma de matrices: \n{suma} \n\n Resta de Matrices: \n{resta} \n\n Multiplicación de matrices \n{multiplicacion}")
